1230125c30a261e244d3ebbcc2cfea152f9766c8
Visnu Pitiyanuvath <visnup@gmail.com>
2018-03-13 00:29:59 -0700

    rewrite

---
+++ script.js
@@ -0,0 +1,12 @@
+var head = document.createElement('div')
+head.innerHTML = `
+  <meta name="viewport" content="width=device-width" />
+  <link href="style.css" rel="stylesheet" />
+`
+while (head.childNodes.length)
+  document.head.appendChild(head.childNodes[0])
+
+var content = document.querySelector('noscript'),
+    src = marked.setOptions({ smartypants: true })(content.innerText || content.innerHTML)
+
+document.body.innerHTML = `<main>${src}</div>`
